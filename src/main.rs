68eeb235b59498fede9b2ed1cdd0f7649dc4990d
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2022-07-20 11:06:08 +0100

    Start to return a super cool json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rocket::response::content;
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +6,13 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/test")]
+fn test() -> content::RawJson<&'static str> {
+    // content::RawJson("{'hi': 'world'}")
+    content::RawJson("{'title': 'New Years Day', 'date': '2017-01-02', 'notes': 'Substitute day', 'bunting': true}")
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, test])
 }
