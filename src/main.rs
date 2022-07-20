38164a1ccbcb04c5cd2a1f79bec85d85ac8675c0
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2022-07-20 12:27:02 +0100

    Create a struct for our json

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use rocket::response::content;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate reqwest;
+
+use rocket::response::content;
+use std::collections::HashMap;
+use std::error::Error;
+
+struct BankHoliday {
+    title: String,
+    // TODO: make date a special date class
+    date: String,
+    notes: String,
+    bunting: bool,
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -9,24 +20,32 @@ fn index() -> &'static str {
 
 #[get("/test")]
 fn test() -> content::RawJson<&'static str> {
-    blah();
-
-
-    // match body {
-    //     Ok(jsonBody) => content::RawJson(&jsonBody),
-    //     Err(_) => content::RawJson("{ 'message': 'uh-oh' }"),
-    // }
-    //
-    content::RawJson("{ 'hello' : 'world' }")
+    content::RawJson("[{
+        'title': 'New Years Day',
+        'date': '2017-01-02',
+        'notes': 'Substitute day',
+        'bunting': true
+        },
+        {
+        'title': 'Good Friday',
+        'date': '2017-04-14',
+        'notes': '',
+        'bunting': false
+        },
+        {
+        'title': 'Easter Monday',
+        'date': '2017-04-17',
+        'notes': '',
+        'bunting': true
+        }]")
 }
 
-fn blah() -> () {
-    let requestUrl = "https://www.gov.uk/bank-holidays/england-and-wales.json";
-    let body = reqwest::get(requestUrl).await?.text().await?;
+// fn blah() -> () {
+//     let requestUrl = "https://www.gov.uk/bank-holidays/england-and-wales.json";
+//     let body = reqwest::get(requestUrl).await?.text().await?;
 
-    println!("body {:?}", body);
-}
-
+//     println!("body {:?}", body);
+// }
 
 #[launch]
 fn rocket() -> _ {
