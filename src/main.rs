f8ebde08adfb4a56507d2bc9341749a5331beff3
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2022-07-20 17:32:33 +0100

    Tris has fixed eveything, we now get the json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate reqwest;
 
+use rocket::serde::{Deserialize};
+
 use rocket::response::content;
 use std::collections::HashMap;
 use std::error::Error;
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
 struct BankHoliday {
     title: String,
     // TODO: make date a special date class
@@ -13,39 +17,48 @@ struct BankHoliday {
     bunting: bool,
 }
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct RegionalBankHoliday {
+    division: String,
+    events: Vec<BankHoliday>
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
 #[get("/test")]
-fn test() -> content::RawJson<&'static str> {
-    content::RawJson("[{
-        'title': 'New Years Day',
-        'date': '2017-01-02',
-        'notes': 'Substitute day',
-        'bunting': true
-        },
-        {
-        'title': 'Good Friday',
-        'date': '2017-04-14',
-        'notes': '',
-        'bunting': false
-        },
-        {
-        'title': 'Easter Monday',
-        'date': '2017-04-17',
-        'notes': '',
-        'bunting': true
-        }]")
+async fn test() -> String  {
+    println!("test called");
+    let rbh = blah().await;
+    dbg!(rbh.division);
+    // content::RawJson("[{
+    //     'title': 'New Years Day',
+    //     'date': '2017-01-02',
+    //     'notes': 'Substitute day',
+    //     'bunting': true
+    //     },
+    //     {
+    //     'title': 'Good Friday',
+    //     'date': '2017-04-14',
+    //     'notes': '',
+    //     'bunting': false
+    //     },
+    //     {
+    //     'title': 'Easter Monday',
+    //     'date': '2017-04-17',
+    //     'notes': '',
+    //     'bunting': true
+    //     }]")
+    "hi".to_string()
 }
 
-// fn blah() -> () {
-//     let requestUrl = "https://www.gov.uk/bank-holidays/england-and-wales.json";
-//     let body = reqwest::get(requestUrl).await?.text().await?;
-
-//     println!("body {:?}", body);
-// }
+async fn blah() -> RegionalBankHoliday {
+    let requestUrl = "https://www.gov.uk/bank-holidays/england-and-wales.json";
+    reqwest::get(requestUrl).await.unwrap().json::<RegionalBankHoliday>().await.unwrap()
+}
 
 #[launch]
 fn rocket() -> _ {
